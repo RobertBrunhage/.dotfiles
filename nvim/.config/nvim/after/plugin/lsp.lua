1bcbf6f7ac65b8506fdabf20b580acbf5e6f634d
Robert Brunhage <robertbrunhage@gmail.com>
2023-09-09 15:07:40 +0200

    chore: break out completion from lsp file

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,4 @@
 local lsp_config = require("lspconfig")
-local cmp = require("cmp")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
@@ -71,40 +70,8 @@ lsp_config.lua_ls.setup({
 	capabilities = capabilities,
 })
 
-local cmp_mappings = cmp.mapping.preset.insert({
-	["<C-b>"] = cmp.mapping.scroll_docs(-4),
-	["<C-f>"] = cmp.mapping.scroll_docs(4),
-	["<C-y>"] = cmp.mapping.complete(),
-	["<C-e>"] = cmp.mapping.abort(),
-	["<CR>"] = cmp.mapping.confirm({ select = false }),
-})
-
--- disable completion with tab
--- this helps with copilot setup
-cmp_mappings["<Tab>"] = vim.NIL
-cmp_mappings["<S-Tab>"] = vim.NIL
-
----@diagnostic disable-next-line: missing-fields
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "path" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-	},
-	mapping = cmp_mappings,
-})
-
--- Snippets will load from LSP but this makes sure to
--- load local and plugin snippets ASAP
-require("luasnip.loaders.from_vscode").lazy_load()
-
+-- some tooltip for the lsp in bottom right
 require("fidget").setup({})
 
+-- hot reload :)
 require("dart-tools")
