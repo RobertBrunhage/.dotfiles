adccec9391b5bba437ac351f3ccf13f818f328aa
Robert Brunhage <robertbrunhage@gmail.com>
2025-05-03 16:34:56 +0200

    chore: add linux script

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
+git submodule update --init --recursive
+rm ~/.bashrc
+rm ~/.zshrc
 
 cd .dotfiles-personal
 stow --restow -t ~ git
 cd ..
 
-stow --restow -t ~ aerospace
 stow --restow -t ~ bin
 stow --restow -t ~ core
 stow --restow -t ~ nvim
 stow --restow -t ~ tmux
 stow --restow -t ~ zsh
-stow --restow -t ~ yabai
-stow --restow -t ~ skhd
-stow --restow -t ~ alacritty
+
+chsh -s $(which zsh)
