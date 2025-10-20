c0874ec6aad8ea3ac0ed553b035f5f9b6e36426f
Robert Brunhage <robertbrunhage@gmail.com>
2025-10-20 15:03:03 +0200

    chore: add more mac tools

--- scripts/mac/productivity-tools.sh
+++ scripts/mac/productivity-tools.sh
@@ -13,12 +13,25 @@ brew install koekeishiya/formulae/skhd
 brew install discord
 brew install notion
 brew install font-hack-nerd-font
-
+brew install stripe/stripe-cli/stripe
+brew install go
 
 # Install Homebrew cask packages (one per line)
 brew install --cask yubico-authenticator
 
 # Install Ghostty
 brew install --cask ghostty
+
+# Install Raycast
+brew install --cask raycast
+
+# Install Hungrimind cli
+npm install -g @hungrimind/hungrimind-cli
+
+# Install Pnpm with npm
+npm install -g pnpm@latest-10
+
+# Install Vercel cli with pnpm
+pnpm i -g vercel
 
 echo "Productivity tools installed via Homebrew."
